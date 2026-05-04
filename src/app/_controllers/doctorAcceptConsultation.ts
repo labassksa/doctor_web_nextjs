3761f0c31609fe4a1ff1365aca529534d1e37556
unknown <mostafakaram485@gmail.com>
2026-05-04 21:41:22 +0300

    c

--- src/app/_controllers/doctorAcceptConsultation.ts
+++ src/app/_controllers/doctorAcceptConsultation.ts
@@ -6,7 +6,7 @@ export const acceptConsultation = async (consultationId: number) => {
   try {
     const token = localStorage.getItem("labass_doctor_token"); // Replace with your actual token retrieval method
     if (!token) {
-      throw new Error("No token found");
+      throw new Error("No token found"); //,
     }
 
     // Request body for accepting the consultation
